bab6f18729caab54ce8f382bbc5d049303401707
li-chuang <278747148@qq.com>
2016-09-21 00:14:48 +0800

    Create NumberFormatTest.java
    All of them was Format

---
+++ com/lichuang/sep20/NumberFormatTest.java
@@ -0,0 +1,50 @@
+package com.lichuang.Sep20;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+/**
+ * 格式化
+ * @author ASUS
+ *
+ */
+public class NumberFormatTest {
+
+	public static void main(String[] args){
+		System.out.println(decimalFormat("120"));
+	}
+	
+	// NumberFormat类的使用，实际用途与SimpleDateFormat相似，但也有不同。
+	public static void numberFormat(int i){
+        // 得到一个NumberFormat的实例
+        NumberFormat nf = NumberFormat.getInstance();
+        // 设置是否使用分组
+        nf.setGroupingUsed(true);
+        // 设置最大整数位数
+        nf.setMaximumIntegerDigits(4);
+        // 设置最小整数位数
+        nf.setMinimumIntegerDigits(1);
+        // 输出测试语句
+        System.out.println(nf.format(i));
+	}
+	
+	// String也有自己格式化的方法，它的方法与C语言中的语法类似
+	public static void stringFormat(int youNumber){
+	    // 0 代表前面补充0   
+	    // 4 代表长度为4   
+	    // d 代表参数为正数型   
+	    String str = String.format("%04d", youNumber);   
+	    System.out.println(str); // 0001   
+	}
+	
+	//流水号加1后返回，流水号长度为4
+    private static final String STR_FORMAT = "0000"; 
+ 
+    public static String decimalFormat(String liuShuiHao){
+        Integer intHao = Integer.parseInt(liuShuiHao);
+        intHao++;
+        DecimalFormat df = new DecimalFormat(STR_FORMAT);
+        return df.format(intHao);
+    }
+	
+}
