648cf6714f0064147c6942ef5d015cb86aeca43e
li-chuang <278747148@qq.com>
2016-10-17 00:32:40 +0800

    Update NIOUtils.java

--- com/lichuang/Oct16/NIOUtils.java
+++ com/lichuang/Oct16/NIOUtils.java
@@ -9,7 +9,8 @@ import java.nio.channels.FileChannel;
 public class NIOUtils {
 
 	public static void main(String[] args) {
-		copyFile(new File("F:\\3.jpg"),new File("E:\\3.jpg"));
+		// copyFile(new File("F:\\testData"),new File("E:\\testData"));
+		copyFiles(new File("F:\\war3"),new File("E:\\war3"));
 	}
 	
 	/**
@@ -22,20 +23,53 @@ public class NIOUtils {
 			if(!source.exists()){
 				throw new Exception("File not exists");
 			}
+			//得到一个源文件对应的输入通道
 			FileChannel fic = new FileInputStream(source).getChannel();
+			//得到一个目标文件对应的输出通道  
 			FileChannel foc = new FileOutputStream(destination).getChannel();
-			
+			//生成一个1024字节的ByteBuffer实例 
 			ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
 			
 			while(fic.read(byteBuffer) != -1){
-				byteBuffer.flip();
+				byteBuffer.flip();  //准备写  
 				foc.write(byteBuffer);
-				byteBuffer.clear();
+				byteBuffer.clear(); //准备读
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
+	/**
+	 * 2.用NIO复制文件夹
+	 */
+	public static void copyFiles(File source,File destination){
+		try {
+			if(!source.exists()){
+				throw new Exception("File Not Found!!");
+			}
+			if(!destination.exists()){
+				destination.mkdirs();
+			}
+			if(source.isDirectory()){
+				String[] srcList = source.list();
+				for(int i=0;i<srcList.length;i++){
+					String src = source.getPath() + "\\" + srcList[i];
+					String dest = destination.getPath() + "\\" + srcList[i];
+					File subSrc = new File(src);
+					if(subSrc.isDirectory()){
+						copyFiles(subSrc,new File(dest));
+					}else{
+						copyFile(subSrc,new File(dest));
+					}
+					
+				}
+			}
+		} catch (Exception e) {
+		}
+	}
+	
+	
 }
+
 
