c6649705bd4dc5bf64068eb6223ac71f9dfa1a68
li-chuang <278747148@qq.com>
2016-10-17 22:59:43 +0800

    Update NIOUtils.java

--- com/lichuang/Oct16/NIOUtils.java
+++ com/lichuang/Oct16/NIOUtils.java
@@ -2,7 +2,9 @@ package com.lichuang.Oct16;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
@@ -10,7 +12,8 @@ public class NIOUtils {
 
 	public static void main(String[] args) {
 		// copyFile(new File("F:\\testData"),new File("E:\\testData"));
-		copyFiles(new File("F:\\war3"),new File("E:\\war3"));
+		// copyFiles(new File("F:\\war3"),new File("E:\\war3"));
+		readContent(new File("E:\\wen.txt"));  //E:\\logFile_2.txt"
 	}
 	
 	/**
@@ -69,7 +72,27 @@ public class NIOUtils {
 		}
 	}
 	
+	/**
+	 * 3.读取文件内容(英文)
+	 *   
+	 */
+	public static void readContent(File file){
+		try {
+			//第一步：从FileInputStream获取通道
+			FileChannel fc = new FileInputStream(file).getChannel();
+			//第二步：创建缓冲区
+			ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+			//第三步：将数据从通道读到缓冲区中
+			fc.read(byteBuffer);
+			
+			byteBuffer.flip(); // 位置设置为0，在开始的位置做好输出的准备 
+			while(byteBuffer.hasRemaining()){
+				System.out.print((char)byteBuffer.get());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
 }
-
 
