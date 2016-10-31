83ba20f98aaf8cb66039f4543244772cc5a76b4d
li-chuang <278747148@qq.com>
2016-11-01 00:50:18 +0800

    Update GsonUtils.java

--- com/lichuang/Oct31/GsonUtils.java
+++ com/lichuang/Oct31/GsonUtils.java
@@ -1,68 +1,82 @@
 package com.lichuang.Oct31;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 /**
- * Gson的使用 
- * 1.用String类型的json数据生成Json对象； 
- * 2.用Object实体数据生成对应的Json对象；
- * 3.用String类型的json数据生成Object对象；
- * 4.用Json对象生成生成对应的Object对象；
- * 5.用Object对象生成String类型的json数据；
- * 6.用Json对象生成对应的String类型的json数据；
- * 
+ * Gson的使用
  */
 public class GsonUtils {
 
 	public static void main(String[] args) {
-		//3 stringToObject("{\"age\":\"100\",\"name\":\"lich\",\"favorite\":\"football\"}");
-		stringToBean("{\"age\":\"100\",\"name\":\"lich\",\"favorite\":[\"football\",\"swimming\"]}");
-		//5 objectToString(new Person("lich", "100", new String[] { "football","swimming" }));
-	}
+		// stringToObject("{\"age\":\"100\",\"name\":\"lich\",\"favorite\":\"football\"}");
+		// stringToBean("{\"age\":\"100\",\"name\":\"lich\",\"favorite\":[\"football\",\"swimming\"]}");
 
-	// 1.将String类型的信息转换为Json对象
-	public static void stringToJson(String str) {
-
-	}
-
-	// 2.用Object实体数据生成对应的Json对象
-	public static void objectToJson(Object obj) {
-		Gson gson = new Gson();
+		// objectToString(new Person("lich", "100", new String[] {
+		// "football","swimming" }));
 		
+		/*Map<String,String> map= new HashMap<String,String>();
+		map.put("name", "lich");
+		map.put("age", "99");
+		mapToString(map);*/
+		
+		List<String> list = new ArrayList<String>();
+		list.add("football");
+		list.add("swimming");
+		listToString(list);
 	}
 
 	// 3.用String类型的json数据生成Object对象
 	public static void stringToObject(String str) {
 		Gson gson = new Gson();
-		Type type = new TypeToken<Map<String, String>>(){}.getType();
-        Map<String, String> map = gson.fromJson(str, type);//这里的Type很麻烦，类型复杂了解析不了
-        for (String key : map.keySet()) {
-            System.out.println("map.get = " + map.get(key));
-        }
+		Type type = new TypeToken<Map<String, String>>() {}.getType();
+		Map<String, String> map = gson.fromJson(str, type);// 这里的Type很麻烦，类型复杂了解析不了
+		for (String key : map.keySet()) {
+			System.out.println("map.get = " + map.get(key));
+		}
 	}
-	
+
 	public static void stringToBean(String str) {
 		Gson gson = new Gson();
-        Person person = gson.fromJson(str, Person.class);
-        System.out.println(person.getAge()+" "+person.getName()+" "+Arrays.toString(person.getFavorite()));
+		Person person = gson.fromJson(str, Person.class);// fromJson解析还要提供类型
+		System.out.println(person.getAge() + " " + person.getName() + " "
+				+ Arrays.toString(person.getFavorite()));
+	}
+
+	public static void beanToString(Person person) {
+		Gson gson = new Gson();
+		String gsonStr = gson.toJson(person);// 注意，toJson的参数Object范围极广，集合类完全没有问题
+		System.out.println(gsonStr);
+	}
+	
+	public static void mapToString(Map<String,String> map){
+		Gson gson = new Gson();
+		String jsonStr = gson.toJson(map);
+		System.out.println(jsonStr);
+	}
+	
+	public static void listToString(List<String> list){
+		Gson gson = new Gson();
+		String gsonStr = gson.toJson(list);
+		System.out.println(gsonStr);
+	}
+
+	// 2.用Object实体数据生成对应的Json对象
+	public static void objectToJson(Object obj) {
+		Gson gson = new Gson();
+
 	}
 
 	// 4.用Json对象生成生成对应的Object对象
 	public static void jsonToObject(Gson json) {
 
-	}
-
-	// 5.用Object对象生成String类型的json数据
-	public static void objectToString(Object obj) {
-		Person person = (Person) obj;
-		Gson gson = new Gson();
-		String gsonStr = gson.toJson(person);// 注意，toJson的参数Object范围极广，集合类完全没有问题
-		System.out.println(gsonStr);
 	}
 
 	// 6.用Json对象生成对应的String类型的json数据
