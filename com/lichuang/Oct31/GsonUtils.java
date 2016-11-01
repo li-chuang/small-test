63557313082427c70b64e358b4c51d4a85f36981
li-chuang <278747148@qq.com>
2016-11-01 23:29:00 +0800

    Update GsonUtils.java

--- com/lichuang/Oct31/GsonUtils.java
+++ com/lichuang/Oct31/GsonUtils.java
@@ -1,15 +1,11 @@
 package com.lichuang.Oct31;
 
 import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
 
